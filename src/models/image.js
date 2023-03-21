c457d99924cd07b3284ce41372cc53d622136f75
WinyRangel <99991728+WinyRangel@users.noreply.github.com>
2023-03-21 13:23:18 -0600

    Add files via upload

---
+++ src/models/image.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+import path from "path";
+import mongooseLeanVirtuals from "mongoose-lean-virtuals";
+
+const imageSchema = new Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, trim: true },
+    filename: { type: String },
+    views: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },
+    timestamp: { type: Date, default: Date.now },
+    verified:{type: Boolean, default: false},
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+  }
+);
+
+imageSchema.plugin(mongooseLeanVirtuals);
+
+imageSchema.virtual("uniqueId").get(function () {
+  return this.filename.replace(path.extname(this.filename), "");
+});
+
+export default model("Image", imageSchema);
